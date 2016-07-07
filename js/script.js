dcde52dbfbac76af6915d0994dfc08bf3ac3a13e
Shelby Clayton <shelby_clayton@hotmail.com>
2016-07-06 21:13:09 -0700

    add a functioning page with accurate return data.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
-  $('#blanks form').sumbit(function(event) {
+
+
+  $('#blanks form').submit(function(event) {
+    event.preventDefault();
     var typeInput = $('input#type').val();
     var beverageInput = $("input#beverage").val();
     var foodInput = $("input#food").val();
     var entertainmentInput = $("input#entertainment").val();
     var weatherInput = $("input#weather").val();
-  event.preventDefault();
 
-  alert('typeInput');
+    if (typeInput === 'beach' && beverageInput === 'cocktails'|| foodInput === 'cheeseburger' && weatherInput === '90s') {
+      $('#mexico').show();
+    }
+    else if (entertainmentInput === 'hiking') {
+      $('#yellowstone').show();
+    }
+    else if (weatherInput === 'freezing') {
+      $('#aspen').show();
+    }
+    else if (foodInput === 'pasta') {
+      $('#paris').show();
+    } else {
+      $('#stumped').show();
+    }
   });
-
-  if (typeInput === 'beach') {
-    $('#mexico').show();
-  }
-  else if (entertainmentInput === 'hiking') {
-    $('#yellowstone').show();
-  }
-  else if (weatherInput === 'freezing') {
-    $('#aspen').show();
-  }
-  else if (entertainmentInput === 'fruit and cheese') {
-    $('#paris')
-  } else {
-    $('#stumped').show();
-  }
-  alert("end");
 });
